305ed437cad06fb426d11af390c6c1c5469f228a
idankri <58448747+idankri@users.noreply.github.com>
2020-11-10 20:05:17 +0200

    Added files

--- HW2/app/src/main/java/com/example/milab_hw2/MainActivity.java
+++ HW2/app/src/main/java/com/example/milab_hw2/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.milab_hw2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button starks_button = (Button)findViewById(R.id.strarks_button);
+        Button lannisters_button = (Button)findViewById(R.id.lanisters_button);
+
+        starks_button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Toast toast = Toast.makeText(v.getContext(), "Hello!!!!!", Toast.LENGTH_SHORT);
+                toast.show();
+            }
+        });
     }
 }
